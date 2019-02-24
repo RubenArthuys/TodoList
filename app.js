37c2e323bb2d8566d30f7a75697ef4a717b0eb8a
Ruben Arthuys <Ruben@pc57.home>
2019-02-24 22:10:50 +0100

    new version

--- app.js
+++ app.js
@@ -2,29 +2,43 @@ var express = require('express');
 var session = require('cookie-session');
 var bodyParser = require('body-parser');
 var urlencodeParser = bodyParser.urlencoded({ extended: false});
-var path = require ('path');
+// var path = require ('path');
 
 var app = express();
 app.set('view engine', 'ejs');
+// CSS-Js-Static loading
+// app.use(express.static(path.join(__dirname, 'public')));
+// app.set('views', __dirname + '/views');
 
 app.use(session({secret: 'todosecret'}))
 
-app.get('/todo', function(req, res) {
-  res.render('index.ejs', {null:null});
-});
+.use(function(req, res, next) {
+  if (typeof(req.session.todolist) == 'undefined') {
+    req.session.todolist = [];
+  }
+  next();
+})
 
-// app.get('/', function(req, res) {
-//   res.render('', {null:null});
-// });
+.get('/todo', function(req, res) {
+  res.render('index.ejs', {todolist: req.session.todolist});
+})
 
-// app.get('/', function(req, res) {
-//   res.render('', {null:null});
-// });
+.post('/todo/add/', urlencodeParser, function(req, res) {
+  if (req.body.newtodo != '') {
+    req.session.todolist.push(req.body.newtodo);
+  }
+  res.redirect('/todo');
+})
 
+.get('/todo/delete/:id', function(req, res) {
+  if (req.params.id != '') {
+    req.session.todolist.splice(req.params.id, 1);
+  }
+  res.redirect('/todo');
+})
 
+.use(function(req, res, next){
+  res.redirect('/todo');
+})
 
-// CSS-Js-Static loading
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('views', __dirname + '/views');
-
-app.listen(8080);
+.listen(8080);
